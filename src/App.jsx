3f8135796a036e021b5037720ff2633660565717
Amandine Ameye <amandineameye@gmail.com>
2025-02-25 00:46:35 +0100

    Added the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import QuizList from "./components/quiz/QuizList";
+import QuizDetail from "./components/quiz/QuizDetail";
 import CreateForm from "./components/quiz/CreateQuiz";
 
 function App() {
 	return (
 		<>
-			<CreateForm />
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<QuizList />} />
+					<Route path="/quiz/:id" element={<QuizDetail />} />
+					<Route path="/create" element={<CreateForm />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
